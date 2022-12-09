9f3f6ca6ab8a15dd422911115c37df04d194030f
BrownieInMotion <accounts@danielclu.com>
2022-12-09 02:06:14 -0500

    refactor: add comments for day 8

--- source/day-08.hs
+++ source/day-08.hs
@@ -30,10 +30,14 @@ solution c r o f trees = c $ zipWith count (standard trees) (flipped trees)
 
 one :: [[Int]] -> Int
 one = solution sum (length . filter id) (||) f
+    -- takes a list of tree heights and gives a list of booleans
+    -- each boolean represents whether a tree is visible from the head
     where f = ((-1 :) . scanl1 max) >>= zipWith (<)
 
 two :: [[Int]] -> Int
 two = solution maximum maximum (*) (map (length . seen) . init . tails)
+    -- takes a list of tree heights and shows all visible trees from each tree
+    -- only does so in one direction (down the list)
     where seen = uncurry takeUntil . bimap ((<=) . head) tail . join (,)
 
 main = run parse [show . one, show . two] "../inputs/day-08"
