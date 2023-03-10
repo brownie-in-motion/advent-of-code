d1927da56915e5e714ab6d376f7bfaefff37b7a3
BrownieInMotion <accounts@danielclu.com>
2023-03-10 13:08:23 -0500

    feat: add day 01 solve

--- solutions/day_01.rs
+++ solutions/day_01.rs
@@ -1,12 +1,20 @@
-struct Input {}
+type Input = Vec<u64>;
 
 #[aoc::day(1)]
-fn parse_input(_input: &str) -> Input {
-    Input {}
+fn parse_input(input: &str) -> Input {
+    input.lines().map(|line| line.parse().unwrap()).collect()
+}
+
+fn slide(input: Input, skip: usize) -> usize {
+    input
+        .iter()
+        .zip(input.iter().skip(skip))
+        .filter(|(a, b)| b > a)
+        .count()
 }
 
 #[aoc::solve(1)]
-fn solve_one(_input: Input) -> u8 { 0 }
+fn solve_one(input: Input) -> usize { slide(input, 1) }
 
 #[aoc::solve(2)]
-fn solve_two(_input: Input) -> u8 { 0 }
+fn solve_two(input: Input) -> usize { slide(input, 3) }
