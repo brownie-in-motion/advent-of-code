8a41aee14cbf62bddd486b37e7a348d672c1273d
BrownieInMotion <accounts@danielclu.com>
2023-03-11 01:55:34 -0500

    refactor(day_03): more cleaning up

--- 2021/solutions/day_03.rs
+++ 2021/solutions/day_03.rs
@@ -16,35 +16,27 @@ fn parse_input(input: &str) -> Problem {
         .collect()
 }
 
-fn compare(input: &Problem) -> Vec<bool> {
-    let total = input.len();
-    let width = input[0].len();
-    let result = input.iter().fold(vec![0; width], |acc, line| {
-        acc.iter()
-            .zip(line)
-            .map(|(n, c)| n + *c as usize)
-            .collect::<Vec<usize>>()
-    });
-    result.iter().map(|n| (n * 2 >= total)).collect()
-}
-
-fn compare_column(input: &Problem, i: usize) -> bool {
-    let ones = input.iter().filter(|row| row[i]).count();
-    ones * 2 >= input.len()
-}
-
 fn decode(binary: &Vec<bool>) -> u64 {
     binary.iter().fold(0, |acc, n| (acc << 1) + *n as u64)
 }
 
 #[aoc::solve(1)]
 fn solve_one(input: Problem) -> u64 {
-    let best = compare(&input);
+    let total = input.len();
+    let width = input[0].len();
+    let result = input.iter().fold(vec![0; width], |acc, line| {
+        acc.iter().zip(line).map(|(n, c)| n + *c as usize).collect()
+    });
+    let best = result.iter().map(|n| (n * 2 >= total)).collect();
     let value = decode(&best);
-    value * (value ^ (1 << best.len()) - 1)
+    value * (value ^ (1 << width) - 1)
 }
 
-fn prune(i: usize, target: bool, values: Vec<Vec<bool>>) -> Vec<Vec<bool>> {
+fn max_column(input: &Problem, i: usize) -> bool {
+    input.iter().filter(|row| row[i]).count() * 2 >= input.len()
+}
+
+fn prune(i: usize, target: bool, values: Problem) -> Problem {
     match values.len() {
         1 => values,
         _ => values
@@ -61,8 +53,8 @@ fn solve_two(input: Problem) -> u64 {
     let full = (input.clone(), input);
     let (most, least) = (0..width).fold(full, |(most, least), i| {
         (
-            prune(i, compare_column(&most, i), most),
-            prune(i, !compare_column(&least, i), least),
+            prune(i, max_column(&most, i), most),
+            prune(i, !max_column(&least, i), least),
         )
     });
     decode(&most[0]) * decode(&least[0])
